e3d85325298bd91378d927b52b24a066f73e6d72
alisoneoz <alisonestephanyoz@gmail.com>
2023-04-05 16:30:02 -0400

    Add metadata tags

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,15 @@
-import Link from "next/link"; // 1. import the Link element from 'next-link'
+import Head from "next/head"; // TO ADD METADATA --> 1. import the Head component from next/head
+import Link from "next/link"; //TO ADD LINKS --> 1. import the Link component from 'next-link'
 
 export default function FirstPost() {
   return (
     <>
+      <Head> {/* TO ADD METADATA --> 2. Add the Head component  */}
+        <title>First Post</title> {/* TO ADD METADATA --> 3. Include the metadata tags that you need  */}
+      </Head>
       <h1>First Post</h1>
       <h2>
-        <Link href="/">Back to home</Link> {/* 2. Add the Link component  */}
+        <Link href="/">Back to home</Link> {/* TO ADD LINKS --> 2. Add the Link component  */}
       </h2>
     </>
   );
